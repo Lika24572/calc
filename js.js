a05dee90570773759cec82697ff98d743f588653
Lika24572 <likaorenburg@gmail.com>
2024-11-20 16:43:08 +0300

    js.js

--- js.js
+++ js.js
@@ -1,100 +1,49 @@
-const result = document.querySelector('#result'),
-      expression = document.querySelector('#expression'),
-      num = document.querySelectorAll('.number:not(.equals)'),
-      operation = document.querySelectorAll('.operation'),
-      equals = document.querySelector('.equals'),
-      clear = document.querySelector('#clear'),
-      ce = document.querySelector('#ce');
-let ex = ''; // the expression string to be eval'd
-result.innerHTML = '0';
-
-
-
-function clickN() { // when we click on a number
-  if(!ex  typeof(ex) === 'number'  ex === '0') {
-    expression.innerHTML = this.id;
-    ex = this.id;
-  } else {
-    expression.innerHTML += this.id;
-    ex += this.id;
-  }
-  result.innerHTML = ex.split(/\/|\*|\+|-|=/).pop();
-  checkLength(result.innerHTML);
-};
-
-function clickO() { // when we click on an operation
-  if(!ex) {
-    return;
-  }
-  ex = ex.toString().replace(/=/, '');
-  if (ex.match(/\/|\*|\+|-|=/)) {
-    ex = eval(ex).toString();
-  } 
-  expression.innerHTML = expression.innerHTML.replace(/=/, '') + this.id;
-  ex += this.id;
-  result.innerHTML = this.id;
-};
-
-
-
-Array.from(num).forEach(function(element) { // assign appropriate function to all numbers and operations
-      element.addEventListener('click', clickN);
-    });
-
-Array.from(operation).forEach(function(element) {
-      element.addEventListener('click', clickO);
-    });
-
-// clear all on click
-clear.addEventListener('click', () => {
-  result.innerHTML = '';
-  expression.innerHTML = '';
-  ex = '';
-})
-
-// clear last entry on click
-ce.addEventListener('click', () => {
-  if (!expression.innerHTML.match(/=$/)) {
-    
-    expression.innerHTML = doCE(expression.innerHTML);
-    ex = doCE(ex); 
-    result.innerHTML = 0;
-    
-    function doCE(arg) {
-      arg = arg.split(/([\/\*\+\-\=])/g);
-      arg.splice(-1, 1);
-      return arg.join('');
+import java.util.Scanner;
+ 
+public class Calculator {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+ 
+        // taking input from the user using the Scanner
+        // class
+        System.out.print(
+            "Enter the first and the Second number - ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+ 
+        // selecting the operand for the calculations
+        System.out.print(
+            "Choose and Enter the type of operation you want to perform (+, -, *, /, %) - ");
+        char op = sc.next().charAt(0);
+        solve(a, b, op);
     }
-  }
-})
-
-// calculate the whole thing
-equals.addEventListener('click', ()=> {
-  if (!ex) {
-    result.innerHTML = '0';
-  } else {
-    ex = eval(ex);
-    expression.innerHTML += '=';
-    result.innerHTML = trim12(ex);
-  }
-})
-
-function checkLength(arg) { // if we enter a number that's too long 
-  if (arg.toString().length > 14) {
-    expression.innerHTML = 'number too long'.toUpperCase();
-    result.innerHTML = '0';
-    ex = '0';
-  } 
-}
-
-function trim12(arg) { // if we calculate a number that's too long
-  if (arg.toString().length > 14) {
-    ex = parseFloat(arg.toPrecision(12));
-    if (ex.toString().length > 14) { 
-      ex = ex.toExponential(9);
-    };
-    return ex;
-  } else {
-    return arg;
-  }
+    public static int solve(int a, int b, char op)
+    {
+        int ans = 0;
+        // addition
+        if (op == '+') {
+            ans = a + b;
+            // subtraction
+        }
+        else if (op == '-') {
+            ans = a - b;
+            // multiplication
+        }
+        else if (op == '*') {
+            ans = a * b;
+            // modulo
+        }
+        else if (op == '%') {
+            ans = a % b;
+            // division
+        }
+        else if (op == '/') {
+            ans = a / b;
+        }
+ 
+        // printing the final result
+        System.out.println("Your answer is - " + ans);
+        return ans;
+    }
 }
